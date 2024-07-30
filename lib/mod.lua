0e2da925619043b29981e8251c94d94ff72bad7a
Casey Childers <cachilders@gmail.com>
2024-07-30 04:46:17 +0100

    [chore] cleanup scaffolding

--- lib/mod.lua
+++ lib/mod.lua
@@ -1,24 +1,33 @@
 local mod = require 'core/mods'
 
-local api = {}
+local changez = {}
+local flags = {}
 local menu = {}
 local programs = {}
+local public = {}
 
 -- SYSTEM HOOK CALLBACKS
 mod.hook.register('system_post_startup', 'Changez post startup function', function() end)
 -- system_post_startup - called after matron has fully started and system state has been restored but before any script is run
 mod.hook.register('system_pre_shutdown', 'Changez pre shutdown function', function() end)
 -- system_pre_shutdown - called when SYSTEM > SLEEP is selected from the menu
-mod.hook.register('system_pre_init', 'Changez pre init function', function() end)
+mod.hook.register('script_pre_init', 'Changez pre init function', function() end)
 -- script_pre_init - called after a script has been loaded but before its engine has started, pmap settings restored, and init() function called
-mod.hook.register('system_post_init', 'Changez post init function', function() end)
+mod.hook.register('script_post_init', 'Changez post init function', function() end)
 -- script_post_init - called after a script’s init() function has been called
-mod.hook.register('system_post_post_cleanup', 'Changez post cleanup function', function() end)
+mod.hook.register('script_post_cleanup', 'Changez post cleanup function', function() end)
 -- script_post_cleanup - called after a script’s cleanup() function has been called, this normally occurs when switching between scripts
 
--- MENU INSTANTIATION
-menu.key = function(n,d)
-  if n == 2 and z == 0 then
+changez.init = function()
+  -- connect midi and whatnot
+end
+
+changez.on_midi_message = function()
+  -- process incoming message
+end
+
+menu.key = function(k, z)
+  if k == 2 and z == 0 then
     mod.menu.exit()
   end
 end
@@ -44,12 +53,12 @@ end
 
 mod.menu.register(mod.this_name, menu)
 
--- PROVIDE EXTERNAL API
-api.example = function(s)
+-- EXTERNAL API
+public.example = function(s)
   -- method that another script could use
   -- local example = require 'changez/lib/mod'
   -- example('Test api consumption')
   print(s)
 end
 
-return api
+return public
